e0a86e72e3f4468b32e679c9b2fa83fd74a702ab
Asdow <20314541+Asdow@users.noreply.github.com>
2021-12-16 20:49:55 +0200

    Changed rendering batch scripts to use JA2-BatchRender.exe
    Allows automatic parallel rendering by calling blender depending on how many generated script files are present and gets rid of most of the horrible batch script

--- batchrender - NoWeapons.bat
+++ batchrender - NoWeapons.bat
@@ -2,41 +2,11 @@
 setlocal enabledelayedexpansion
 
 rem change these to wherever they are located on your machine
-set _BLENDERDIR=J:\Ohjelmat\Blender\Blender 3.0\
-set _BLENDFILEDIR=J:\JA2 1.13 SVN\JA2-Character-Animation-Photobooth\JA2 2.9_033.blend
-set _PYTHONFILE=J:\JA2 1.13 SVN\JA2-Character-Animation-Photobooth\renderGeneratedScripts\batchrender-noWeaponAnims
+set _BLENDERDIR=J:\\Ohjelmat\\Blender\\Blender 3.0
+set _BLENDFILE=JA2 2.9_033.blend
+set _PYTHONFILE=batchrender-noWeaponAnims.py
 
 
-set /A parallelRender=0
-if !parallelRender!==1 (
-	rem set /p decision=Input amount of blender processes: 
-	set /a decision=8
-)
+JA2-BatchRender.exe "%_BLENDERDIR%" "%_BLENDFILE%" "%_PYTHONFILE%"
 
-
-echo Rendering no weapons animations
-echo opening blender and starting rendering
-cd !_BLENDERDIR!
-
-
-rem 1> nul ==> suppress text output <- Not using this makes headless rendering considerably slower than just doing CTRL + F12 in blender.
-rem 2> nul ==> suppress error output
-if !parallelRender!==0 (
-	blender.exe 1> nul -b "!_BLENDFILEDIR!" -P "!_PYTHONFILE!0.py"
-) ELSE (
-	set /a x=!decision! - 1
-	for /l %%n in (0,1,!x!) do (
-		start /B blender.exe 1> nul 2> nul -b "!_BLENDFILEDIR!" -P "!_PYTHONFILE!%%n.py"
-	)
-
-
-	:LOOP
-	tasklist /FI "IMAGENAME eq blender.exe" 2>NUL | find /I /N "blender.exe">NUL
-	if %ERRORLEVEL%==0 (
-		ping localhost -n 6 >nul
-		GOTO LOOP
-	)
-)
-
-echo rendering complete!
 pause
--- createBatchRenderScripts.bat
+++ createBatchRenderScripts.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
+DEL "renderGeneratedScripts\*.py"
+
 JA2-BatchRenderCreator.exe "1" "noWeaponAnims.txt" "batchrender-unified.py"
-JA2-BatchRenderCreator.exe "4" "meleeWeaponAnims.txt" "batchrender-unified.py"
-JA2-BatchRenderCreator.exe "8" "pistolAnims.txt" "batchrender-unified.py"
+JA2-BatchRenderCreator.exe "1" "meleeWeaponAnims.txt" "batchrender-unified.py"
+JA2-BatchRenderCreator.exe "1" "pistolAnims.txt" "batchrender-unified.py"
 JA2-BatchRenderCreator.exe "1" "rifleAnims.txt" "batchrender-unified.py"
-JA2-BatchRenderCreator.exe "2" "HeavyWeaponAnims.txt" "batchrender-unified.py"
+JA2-BatchRenderCreator.exe "1" "HeavyWeaponAnims.txt" "batchrender-unified.py"
 JA2-BatchRenderCreator.exe "1" "FemaleAnims.txt" "batchrender-unified.py"
